899d1e7fa643a107e1612b32bf9f1290d2083425
ZvonimirSun <hi@iszy.xyz>
2024-07-26 17:42:46 +0800

    chore: format

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+// eslint.config.js
+import antfu from '@antfu/eslint-config'
+// import autoImport from './.eslintrc-auto-import.json' assert {type: 'json'}
+
+export default antfu(
+  {
+    // unocss: true,
+    // ignores: ['src/assets/**/*', 'src/utils/**/*.js'],
+  },
+  // {
+  //   languageOptions: {
+  //     globals: autoImport.globals,
+  //   },
+  // },
+  // {
+  //   rules: {
+  //     'no-console': 'off',
+  //     'curly': 'off',
+  //     'eslint-comments/no-unlimited-disable': 'off',
+  //     'no-restricted-globals': 'off',
+  //     'ts/no-this-alias': 'off',
+  //   },
+  // },
+)
